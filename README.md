9d0548cb2acef6d20263fad1a5f508c95b573ee5
Christopher Sauer <sauer@mfk.fau.de>
2022-03-01 21:22:00 +0100

    generate_plots: Added for github actions

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # COVID-19 Impfungen im Landkreis Weißenburg Gunzenhausen
+anhand aktueller RKI-Daten.
 
-## Binder starten
+## Verlauf Impfungen seit 01.Februar 2022
+![](stacked-bars.png)
+
+## Impfungen gestern Verteilung nach Altersgruppen
+![](daily-numbers.png)
+
+## Impfungen gestern Verteilung nach Impfschutz
+![](daily-counts.png)
+
+## Binder Jupyter Notebook
 [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/defaltus/COVID-19-Impfungen_in_WUG/master)
---
+++ generate_plots.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import plotly.express as px
+
+
+# Get data from RKI Github
+impfungen = pd.read_csv("Aktuell_Deutschland_Landkreise_COVID-19-Impfungen.csv", low_memory=False)
+# https://datengui.de/statistik-erklaert/ags
+ags = '09577'  # NM: 09373, WUG: 09577
+# Create dataframe
+df = impfungen.loc[impfungen["LandkreisId_Impfort"] == ags].groupby(['Impfdatum', 'Impfschutz', 'Altersgruppe'], as_index=False).sum()
+# Get latest datum
+datum = df['Impfdatum'].values[-1]
+
+# Stacked bars plot
+fig = px.bar(df, x='Impfdatum', y='Anzahl', color='Altersgruppe',
+             pattern_shape='Impfschutz', barmode='stack',
+             pattern_shape_sequence=[".", "x", "+"])
+fig.update_xaxes(range=['2022-02-01', datum])
+fig.write_image('stacked-bars.png')
+
+# Pie chart total numbers
+day = df.where(df['Impfdatum'] == datum).dropna()
+fig = px.pie(day, values='Anzahl', names='Altersgruppe')
+fig.write_image('daily-numbers.png')
+
+# Pie chart count of dose
+fig = px.pie(day, values='Anzahl', names='Impfschutz')
+fig.write_image('daily-counts.png')
